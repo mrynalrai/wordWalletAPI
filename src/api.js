e8ee1cc7e9c52b52c51380170ff83f683d64636d
mrynalrai <mrynalrai@gmail.com>
2023-01-03 18:54:25 -0500

    cors fix

--- src/api.js
+++ src/api.js
@@ -66,23 +66,23 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(cors());
+// app.use(cors());
 
-app.use(
-  cors({
-    credentials: true,
-    origin: true,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: 'https://wordwallet-private.netlify.app/',
+//   })
+// );
 
-// https://stackoverflow.com/questions/52942286/comparing-implementing-app-usecorscorsoptions-and-app-options-cors
-app.options(
-  '*',
-  cors({
-    credentials: true,
-    origin: true,
-  })
-);
+// // https://stackoverflow.com/questions/52942286/comparing-implementing-app-usecorscorsoptions-and-app-options-cors
+// app.options(
+//   '*',
+//   cors({
+//     credentials: true,
+//     origin: 'https://wordwallet-private.netlify.app/',
+//   })
+// );
 
 // Limit requests from same API
 // const limiter = rateLimit({
