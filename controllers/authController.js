fa39bf647d86ff08a0b6ddf4c216350972646a38
mrynalrai <mrynalrai@gmail.com>
2023-01-04 13:01:13 -0500

    [FIX] On logout, set cookie to loggedout

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,7 +68,7 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = (req, res) => {
-  res.cookie('jwt', '', {
+  res.cookie('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
     sameSite: 'none',
